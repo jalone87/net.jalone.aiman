2281d178ad06adeceed616167ded38a4e4cf20f0
jalone87 <lorenzo.togna@gmail.com>
2013-05-09 14:40:15 +0200

    gitignore e logger added

---
+++ AiMan/src/aiman/pacs/behaviours/LearningLogger.java
@@ -0,0 +1,51 @@
+package aiman.pacs.behaviours;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import net.jalone.jul4j.logging.Logger;
+
+/** log utility class. logs are printed in home directory */
+public class LearningLogger extends Logger{
+	
+	//SINGLETON
+	private static LearningLogger instance = null;
+	public static Logger getInstance(){
+		if(instance == null){
+			instance = new LearningLogger();
+		}
+		return instance;
+	}
+	
+	private boolean enabled = true;
+	
+	PrintWriter		logger; 
+	
+	protected LearningLogger(){
+		super();
+    	
+    	StackTraceElement[] stack = Thread.currentThread ().getStackTrace ();
+        StackTraceElement main = stack[stack.length - 1];
+        String mainClass = main.getClassName ();
+		try {
+			logger = new PrintWriter(System.getProperty("user.home") + "/" + mainClass +".log", "UTF-8");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void log(){
+		if(enabled)
+	        logger.println("");  
+			logger.flush();
+	}
+	public void log(String s){
+		if(enabled)
+	        logger.println(s);  
+			logger.flush();
+	}
+	
+}
